f050c9832f5c1f2549942aafdf7335f26b2cd525
Anton Vikulov <anton.vikulov@stud.h-da.de>
2024-05-22 21:53:28 +0000

    initial server and client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,6 +1,6 @@
 // A command line chat client will probably need some styling / high-level interaction with the command line. So this
 // project includes `crossterm` by default, a Rust library for cross-plattform command line manipulation
-
+/*
 use crossterm::{
     style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
     ExecutableCommand,
@@ -20,4 +20,63 @@ fn main() {
         .unwrap()
         .execute(ResetColor)
         .unwrap();
+}
+*/
+
+use crossterm::{
+    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
+    ExecutableCommand,
+};
+use futures::select;
+use futures::FutureExt;
+
+use async_std::{
+    io::{stdin, BufReader},
+    net::{TcpStream, ToSocketAddrs},
+    prelude::*,
+    task,
+};
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+
+pub(crate) fn main() -> Result<()> {
+    task::block_on(try_main("127.0.0.1:8888"))
+}
+
+async fn try_main(addr: impl ToSocketAddrs) -> Result<()> {
+    let stream = TcpStream::connect(addr).await?;
+    let (reader, mut writer) = (&stream, &stream);
+    let reader = BufReader::new(reader);
+    let mut lines_from_server = futures::StreamExt::fuse(reader.lines());
+
+    let stdin = BufReader::new(stdin());
+    let mut lines_from_stdin = futures::StreamExt::fuse(stdin.lines());
+    loop {
+        select! {
+            line = lines_from_server.next().fuse() => match line {
+                Some(line) => {
+                    let _line = line?;
+                    std::io::stdout()
+                    .execute(SetForegroundColor(Color::Black))
+                    .unwrap()
+                    .execute(SetBackgroundColor(Color::White))
+                    .unwrap()
+                    .execute(Print(format!("{}\n", _line)))
+                    .unwrap()
+                    .execute(ResetColor)
+                    .unwrap();
+                },
+                None => break,
+            },
+            line = lines_from_stdin.next().fuse() => match line {
+                Some(line) => {
+                    let line = line?;
+                    writer.write_all(line.as_bytes()).await?;
+                    writer.write_all(b"\n").await?;
+                }
+                None => break,
+            }
+        }
+    }
+    Ok(())
 }
